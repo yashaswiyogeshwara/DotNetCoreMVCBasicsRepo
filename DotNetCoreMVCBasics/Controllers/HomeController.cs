d5e3ef51d41968b7cbe1fe62e62c7bad7fa88376
yogey <yashaswi.yogeshwara@campsystems.com>
2021-04-23 21:50:17 +0530

    CookieBasedAuthentication

---
+++ DotNetCoreMVCBasics/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DotNetCoreMVCBasics.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+
+namespace DotNetCoreMVCBasics.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret() {
+            return View();
+        }
+
+        public IActionResult Authenticate()
+        {
+            List<Claim> someClaims = new List<Claim>() {
+                new Claim(ClaimTypes.Name,"Yash"),
+                new Claim(ClaimTypes.Email, "Yash.yoges@gmail.com")
+                };
+            ClaimsIdentity identity = new ClaimsIdentity(someClaims, "CookieAuth");
+            ClaimsPrincipal principal = new ClaimsPrincipal(identity);
+            HttpContext.SignInAsync(principal);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
